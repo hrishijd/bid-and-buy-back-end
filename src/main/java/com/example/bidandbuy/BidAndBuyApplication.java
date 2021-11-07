46329307a0858fda5cf3048cec7a510d525d99f1
hrishijd <hrishi.jadhav24@gmail.com>
2021-11-07 12:16:44 +0530

    initial commit

---
+++ src/main/java/com/example/bidandbuy/BidAndBuyApplication.java
@@ -0,0 +1,13 @@
+package com.example.bidandbuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidAndBuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidAndBuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bidandbuy/BidAndBuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bidandbuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidAndBuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
