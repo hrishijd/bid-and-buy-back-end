fe58d57bc49a37c40f0e377cfc8ce060c547e3fa
hrishijd <hrishi.jadhav24@gmail.com>
2021-12-03 05:17:52 +0530

    updated cors

--- src/main/java/com/example/bidandbuy/configurations/SecurityConfigurations.java
+++ src/main/java/com/example/bidandbuy/configurations/SecurityConfigurations.java
@@ -69,7 +69,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://example.com","localhost:8080","localhost:3000"));
+		configuration.setAllowedOrigins(Arrays.asList("http://example.com","localhost:8080","http://localhost:3000"));
 		configuration.setAllowedMethods(Arrays.asList("GET","POST"));
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
